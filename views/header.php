83684a991c7f03543621c32b12ee9a5cc8374c42
Chanala Wilangka <yaicw49@hotmail.com>
2015-11-06 20:29:27 +0700

    wtf git error commit ???

--- views/header.php
+++ views/header.php
@@ -7,7 +7,7 @@
     <div class="row">
         <div class="twelve columns">
             <nav class="main-nav">
-                <div class="nav-toggle"><i class="fa fa-times"></i></div>
+                <div class="nav-toggle toggle"><i class="fa fa-times"></i></div>
                 <ul>
                     <li><a href="index-th.php">Home<i class="fa fa-angle-right arrow-right"></i></a></li>
                     <li><a href="inaction.php">any i in Action<i class="fa fa-angle-right arrow-right"></i></a></li>
@@ -23,7 +23,7 @@
     </div>
     <div class="section-header s0">
         <!-- Hamburger Res ========= -->
-        <div class="nav-toggle"><i class="fa fa-bars"></i></div>
+        <div class="nav-toggle toggle"><i class="fa fa-bars"></i></div>
         <div class="container">
             <div class="row">
                 <div class="twelve columns">
@@ -44,6 +44,25 @@
     <div class="headerlg-bg">
         <img src="images/header/headerlg-bg.png">
     </div>
+    <div class="row">
+        <div class="twelve columns">
+            <nav class="main-nav-desk">
+                <ul>
+                    <li><a href="index-th.php">Home</a></li>
+                    <li><a href="inaction.php">any i in Action</a></li>
+                    <li><a href="contact.php">Contact Us</a></li>
+                    <li><a href="#">TH</a><span> l </span><a href="#">EN</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</div>
+<!-- Nav Desk scroll ========= -->
+<div class="nav-desk-scroll">
+    <a href="index-th.php">
+        <div class="nav-desk_slogo"><img src="images/content/anyilogo.png"></div>
+    </a>
+    <div class="nav-toggle-sres toggle"><i class="fa fa-bars"></i></div>
     <div class="row">
         <div class="twelve columns">
             <nav class="main-nav-desk">
